1131ea2fcee6cd36479b0246728b8203fcbea147
Pol Estany <polestany@Pols-MacBook-Air.local>
2026-06-15 15:18:01 +0200

    fix Tooltip naming conflict

--- src/modules/FixedIncome/FixedIncome.jsx
+++ src/modules/FixedIncome/FixedIncome.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip as RechartsTooltip, ResponsiveContainer } from 'recharts';
 import Spain from './countries/Spain';
 import './FixedIncome.css';
 
